c5bc2aeca27b14f769f22b5da8ca877a7d0f057d
Anikó Vári <aniko.kovacs.galamb@gmail.com>
2018-10-10 19:09:22 +0200

    5. feladat két paraméter, annyiszor kiírja az első stringet, amennyi a 2 int

--- 2018_10_10_4_feladat/src/Main.java
+++ 2018_10_10_4_feladat/src/Main.java
@@ -4,9 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		int a=Integer.parseInt(args[0]);
 		int i=0;
+		int x=a;
 		for(i=1; i<a+1; i++) {
 			if(a%i==0) {
 				System.out.println(a+" osztója "+i);
+				x--;
 			}
 			
 		}
---
+++ 2018_10_10_5_feladat/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String a=args[0];
+		int b=Integer.parseInt(args[1]);
+		int i=0;
+		while(i<b) {
+			System.out.println(a);
+			i++;
+		}
+
+	}
+
+}
