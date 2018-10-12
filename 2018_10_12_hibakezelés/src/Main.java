97cd817fa9b5c33099697bbb74190a2d7505acfc
Anikó Vári <aniko.kovacs.galamb@gmail.com>
2018-10-12 15:39:15 +0200

    hibakezelés

---
+++ 2018_10_12_hibakezelés/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//try {
+		//	System.out.println(args[0]);
+		//}catch (ArrayIndexOutOfBoundsException e){
+		//	System.err.println("Adj meg értéket a tömbnek!");
+		//}
+
+		try {
+			int a=Integer.parseInt(args[0]);
+			int b=a/0;
+			System.out.println(b);
+			
+		}catch (ArrayIndexOutOfBoundsException e){
+			System.err.println("\nAdj meg értéket a tömbnek!");
+			System.out.println("\n" + e);
+		}catch (ArithmeticException e) {
+			System.err.println("\nNullával osztottál!");
+			System.out.println("\n" + e);
+		}finally {
+			System.out.println("Sok a hibád");
+		}
+	}
+	
+	
+
+}
