1e4164af0a46610f55246acae797cd14e09fdde5
Anikó Vári <aniko.kovacs.galamb@gmail.com>
2018-10-17 19:27:14 +0200

    fx személy regisztráció

---
+++ 2018_10_17_2/src/javaFXApplication/Szemely.java
@@ -0,0 +1,50 @@
+package javaFXApplication;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Szemely {
+
+	private SimpleIntegerProperty id;
+	private SimpleStringProperty vezeteknev;
+	private SimpleStringProperty keresztnev;
+	public final SimpleIntegerProperty idProperty() {
+		return this.id;
+	}
+	
+	public final int getId() {
+		return this.idProperty().get();
+	}
+	
+	public final void setId(final int id) {
+		this.idProperty().set(id);
+	}
+	
+	public final SimpleStringProperty vezeteknevProperty() {
+		return this.vezeteknev;
+	}
+	
+	public final String getVezeteknev() {
+		return this.vezeteknevProperty().get();
+	}
+	
+	public final void setVezeteknev(final String vezeteknev) {
+		this.vezeteknevProperty().set(vezeteknev);
+	}
+	
+	public final SimpleStringProperty keresztnevProperty() {
+		return this.keresztnev;
+	}
+	
+	public final String getKeresztnev() {
+		return this.keresztnevProperty().get();
+	}
+	
+	public final void setKeresztnev(final String keresztnev) {
+		this.keresztnevProperty().set(keresztnev);
+	}
+	
+	
+	
+	
+}
--- 2018_10_17_2/src/javaFXApplication/ViewController.java
+++ 2018_10_17_2/src/javaFXApplication/ViewController.java
@@ -2,24 +2,44 @@ package javaFXApplication;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-/**
- *
- * @author Rédai Dávid
- */
-public class ViewController implements Initializable {
-    
+public class ViewController implements Initializable{
+
     @FXML
     private Label label;
-    
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField textfieldid;
+
+    @FXML
+    private TextField textfieldvezeteknev;
+
+    @FXML
+    private TextField textfieldkeresztnev;
+
+    @FXML
+    private TableView<?> tabla;
+
+    @FXML
+    private TableColumn<?, ?> tablaid;
+
+    @FXML
+    private TableColumn<?, ?> tablavezeteknev;
+
+    @FXML
+    private TableColumn<?, ?> tablakeresztnev;
+
+    @FXML
+    void hozzaadGomb(ActionEvent event) {
+
     }
     
     @Override
